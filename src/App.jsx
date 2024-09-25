271e2339676791b8cb94922e2f178656f7d04740
Tulio Salvatierra <salvacorp@gmail.com>
2024-09-25 13:22:26 -0400

    built basic link template

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Logo from "./assets/musa-logo.png";
 
 function App() {
   return (
@@ -6,24 +7,17 @@ function App() {
       <div className="items">
         <div className="banner">
           <img
-            src="./thumbnails/profile-pic.jpg"
+            src={Logo}
             alt="Tulio Salvatierra profile"
-            className="banner-image"
+            className="logo"
+            style={{ borderRadius: "2rem" }}
           ></img>
+          <p>✨ DISCOVER ✨ CREATE ✨ TRANSFORM ✨</p>
         </div>
-
-        <h1 className="name">Tulio Salvatierra</h1>
 
         <div className="social-icons">
           <a href="https://github.com/T00lio" target="_blank">
-            <i className="fa-brands fa-github fa-1x"></i>
-          </a>
-
-          <a
-            href="https://www.linkedin.com/in/tulio-salvatierra/"
-            target="_blank"
-          >
-            <i className="fa-brands fa-linkedin-in fa-1x"></i>
+            <i className="fa-solid fa-phone fa-1x"></i>
           </a>
 
           <a href="https://www.instagram.com/tulio_dev/" target="_blank">
@@ -36,26 +30,35 @@ function App() {
         </div>
 
         <div className="link">
-          <img className="thumbnail" src="./thumbnails/image.png " />
+          <img
+            className="thumbnail"
+            src="https://media.istockphoto.com/id/1218636286/photo/manicure-process-in-the-professional-nail-salon.jpg?s=1024x1024&w=is&k=20&c=3-se_aPYhDms0O5J2bMtLZC7MLgzRiz4y-PHLBqtLek="
+          />
 
-          <a target="_blank" href="https://tuliosalvatierra.com">
-            Web development portfolio
+          <a target="_blank" href="#">
+            Our services
           </a>
         </div>
 
         <div className="link">
-          <img className="thumbnail" src="./thumbnails/band thumb.png" />
+          <img
+            className="thumbnail"
+            src="https://media.istockphoto.com/id/1409304190/photo/embroidered-red-pins-on-a-calendar-event-planner-calendar-clock-to-set-timetable-organize.jpg?s=1024x1024&w=is&k=20&c=tLbse6o7HSD4yKukcPzzjzllgN4nuGgGaoUvb3gisAE="
+          />
 
-          <a target="_blank" href="https://www.instagram.com/wratheonofficial/">
-            Wratheon: Blackend Trash metal from Chicago, IL
+          <a target="_blank" href="#">
+            Book an appoint now!
           </a>
         </div>
 
         <div className="link">
-          <img className="thumbnail" src="./thumbnails/up.png" />
+          <img
+            className="thumbnail"
+            src="https://media.istockphoto.com/id/1360092910/photo/words-with-about-us-web-concept-idea.jpg?s=1024x1024&w=is&k=20&c=A7p3J3uwa4_5wp9kOoMN4q02Ly05xE2hKRi9VwomSQ4="
+          />
 
-          <a target="_blank" href="https://www.upwork.com/freelancers/tulio">
-            A My freelancer website @ upwork.com
+          <a target="_blank" href="#">
+            About us!
           </a>
         </div>
       </div>
